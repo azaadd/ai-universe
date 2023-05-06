04b1b1ef8d822eda30d5a4cff9f6d73b6260a660
azaadd <abulkalam.kta@gmail.com>
2023-05-06 21:31:20 +0600

    added ai tools modal card description

--- js/app.js
+++ js/app.js
@@ -2,59 +2,60 @@ const loadAiTools = async(dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayTools(data.data.tools, dataLimit);
+    displayCards(data.data.tools, dataLimit);
 }
 
-const displayTools = (tools, dataLimit) =>{
-    const toolsContainer = document.getElementById('tool-container');
+const displayCards = (cards, dataLimit) =>{
+    const cardsContainer = document.getElementById('tool-container');
 
-    toolsContainer.innerHTML = '';
+    cardsContainer.innerHTML = '';
 
     // start spinner loader...
     toggleSpinner(true);
 
     // display 6 Ai card tools only. 
     const showAll = document.getElementById('show-all');
-    if(dataLimit && tools.length > 6){
-        tools = tools.slice(0, 6);
+    if(dataLimit && cards.length > 6){
+        cards = cards.slice(0, 6);
         showAll.classList.remove('d-none');
     }
     else{
         showAll.classList.add('d-none');
     }
 
-    tools.forEach(tool => {
-        const toolDiv = document.createElement('div');
-        toolDiv.classList.add('col');
-        toolDiv.innerHTML = `
-            <div class="card p-3">
-                <img src="${tool.image}" class="card-img-top mx-auto rounded sizes" alt="...">
+    // creating display cards
+    cards.forEach(card => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+            <div class="h-100 w-auto card p-4">
+                <img src="${card.image}" class="card-img-top mx-auto rounded sizes" alt="...">
                 <div class="card-body">
                     <h5 class="card-title mb-4">Features</h5>
-                    <span class="card-text">1. ${tool.features[0]}</span><br>
-                    <span class="card-text">2. ${tool.features[1]}</span><br>
-                    <span class="card-text">3. ${tool.features[2]}</span><br>
+                    <span class="card-text">1. ${card.features[0]}</span><br>
+                    <span class="card-text">2. ${card.features[1]}</span><br>
+                    <span class="card-text">3. ${card.features[2]}</span><br>
                 </div>
                 <hr class="mx-3">
                 <div class="d-flex  justify-content-between">
                     <div>
-                        <h5 class="list-group-item mb-3 ms-3">${tool.name}</h5>
+                        <h5 class="list-group-item mb-3 ms-3">${card.name}</h5>
                         <div class="ms-3">
                             <i class="fa-regular fa-calendar"></i>
-                            <span class="ms-2">${tool.published_in}</span>
+                            <span class="ms-2">${card.published_in}</span>
                         </div>
                     </div>
-                    <button onclick="loadAiToolDetails(${tool.id})" class="btn btn-light rounded-4 me-3 h-25 mt-3" data-bs-toggle="modal" data-bs-target="#toolDetailModal"><i class="fa-solid red fa-arrow-right"></i></button>
+                    <button onclick="loadAiToolDetails('${card.id}')" class="btn btn-light rounded-5 me-3 h-75 mt-3 text-center" data-bs-toggle="modal" data-bs-target="#toolDetailModal"><i class="fa-solid red fa-arrow-right text-center"></i></button>
                     
                 </div>
             </div>
         `;
-        toolsContainer.appendChild(toolDiv);
+        cardsContainer.appendChild(cardDiv);
     });
 
     // stop spinner loader...
     toggleSpinner(false);
-}
+};
 
 
 const toggleSpinner = isLoading =>{
@@ -73,11 +74,20 @@ document.getElementById('btn-show-all').addEventListener('click', function(){
 })
 
 
-const loadAiToolDetails = async id =>{
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+const loadAiToolDetails = async (id)=>{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.tools);
+    displayToolDetail(data.data);
+}
+
+
+const displayToolDetail = (tool) =>{
+
+    
+        const modalCardDescription = document.getElementById('card-description');
+        modalCardDescription.innerText = `${tool.description ? tool.description : "No description found"}`;
+                        
 }
 
 loadAiTools(6);
